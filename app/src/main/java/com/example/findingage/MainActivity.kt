a6a76dbae9fc6ebcb9fbed3a7fa2c0cbda65913e
Vince Reginald Eugenio <vre4252000@gmail.com>
2023-07-25 05:10:34 +0800

    Implemented a functioning button

--- app/src/main/java/com/example/findingage/MainActivity.kt
+++ app/src/main/java/com/example/findingage/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.findingage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import com.example.findingage.databinding.ActivityMainBinding
+import java.util.Calendar
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.txt)
-        textView.text = "Hello Kotlin"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.setOnClickListener {
+            getAge()
+        }
+    }
+
+    fun getAge(){
+        val userDOB = Integer.parseInt(binding.etd.text.toString())
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val usersAgeinYears = currentYear - userDOB
+        binding.age.text = "Your age is $usersAgeinYears years old"
     }
 }
